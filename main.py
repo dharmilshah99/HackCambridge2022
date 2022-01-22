0d0ad5ab027d7b6629ffdc341a430eaf876c99b0
dharmilshah99 <dharmil.shah@hotmail.com>
2022-01-22 18:51:06 +0000

    Adds Wordcloud Support

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
-from deepgram import Deepgram
 import asyncio, json
 
+from wordcloud import WordCloud
+from deepgram import Deepgram
+from matplotlib import pyplot as plt
 from config import *
 
 ###
@@ -20,8 +22,15 @@ async def get_inference(audio_bytestream):
     """Gets transcription of an audio byte stream."""
     source = {'buffer': audio_bytestream, 'mimetype': 'audio/wav'}
     response = await DG_CLIENT.transcription.prerecorded(source, {'punctuate': True})
-    print(json.dumps(response, indent=4))
+    return response["results"]["channels"][0]["alternatives"][0]["transcript"]
 
+def wordcloud_generator(audio_transcript):
+    """Generate wordcloud image."""
+    wordcloud_image = WordCloud(
+        background_color="white"
+    ).generate(audio_transcript)
+    print(wordcloud_image)
+    return wordcloud_image
 
 ###
 # Streamlit App
@@ -31,4 +40,9 @@ st.set_page_config(layout="centered", page_icon="💬", page_title="Audio Cloud"
 st.title("Audio Cloud")
 uploaded_file = st.file_uploader(label="Upload Audio Recording", )
 if uploaded_file is not None:
-    asyncio.run(get_inference(uploaded_file.getvalue()))
+    audio_transcript = asyncio.run(get_inference(uploaded_file.getvalue()))
+    wordcloud = wordcloud_generator(audio_transcript)
+    fig = plt.figure()
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    st.pyplot(fig)
