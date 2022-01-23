3d93a75fbc4feea38e582353981930d3b1e3a29e
jf-eng <engjianfu@gmail.com>
2022-01-23 02:33:44 +0000

    first working component

--- template-reactless/my_component/__init__.py
+++ template-reactless/my_component/__init__.py
@@ -87,8 +87,9 @@ if not _RELEASE:
 
     # Create an instance of our component with a constant `name` arg, and
     # print its output value.
-    num_clicks = my_component("World")
-    st.markdown("You've clicked %s times!" % int(num_clicks))
+    transcript = my_component("World")
+    st.write(transcript)
+    # st.markdown("You've clicked %s times!" % int(num_clicks))
 
     st.markdown("---")
     st.subheader("Component with variable args")
@@ -97,10 +98,10 @@ if not _RELEASE:
     # based on a text_input widget.
     #
     # We use the special "key" argument to assign a fixed identity to this
-    # component instance. By default, when a component's arguments change,
+    # component instance. By default, when a  component's arguments change,
     # it is considered a new instance and will be re-mounted on the frontend
     # and lose its current state. In this case, we want to vary the component's
     # "name" argument without having it get recreated.
     name_input = st.text_input("Enter a name", value="Streamlit")
-    num_clicks = my_component(name_input, key="foo")
-    st.markdown("You've clicked %s times!" % int(num_clicks))
+    transcript = my_component(name_input, key="foo")
+    st.write(transcript)
--- template-reactless/my_component/frontend/src/index.tsx
+++ template-reactless/my_component/frontend/src/index.tsx
@@ -13,8 +13,49 @@ let isFocused = false
 button.onclick = function(): void {
   // Increment numClicks, and pass the new value back to
   // Streamlit via `Streamlit.setComponentValue`.
-  numClicks += 1
-  Streamlit.setComponentValue(numClicks)
+  // numClicks += 1
+  // Streamlit.setComponentValue(numClicks)
+  navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+    console.log({ stream })
+    if (!MediaRecorder.isTypeSupported('audio/webm'))
+      return alert('Browser not supported')
+    const mediaRecorder = new MediaRecorder(stream, {
+      mimeType: 'audio/webm',
+    })
+    const socket = new WebSocket('wss://api.deepgram.com/v1/listen', [
+      'token',
+      '7e3ea4dfb0238df8ba87d72fc46ed37e19984beb',
+    ])
+    socket.onopen = () => {
+      console.log({ event: 'onopen' })
+      mediaRecorder.addEventListener('dataavailable', async (event) => {
+        if (event.data.size > 0 && socket.readyState == 1) {
+          socket.send(event.data)
+        }
+      })
+      mediaRecorder.start(1000)
+    }
+    
+    let liveFeed = ''
+    socket.onmessage = (message) => {
+      const received = JSON.parse(message.data)
+      const transcript = received.channel.alternatives[0].transcript
+      if (transcript && received.is_final) {
+        console.log(transcript)
+        liveFeed+=
+          transcript + ' '
+        Streamlit.setComponentValue(liveFeed)
+      }
+    }
+
+    socket.onclose = () => {
+      console.log({ event: 'onclose' })
+    }
+
+    socket.onerror = (error) => {
+      console.log({ event: 'onerror', error })
+    }
+  })
 }
 
 button.onfocus = function(): void {
